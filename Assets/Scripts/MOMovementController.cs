fd34944d8e905e268df55b5bd7be18f3bc07d940
Myles Okorn <1009099@student.sae.edu.au>
2018-07-02 15:28:44 +1000

    player input and entity controller scripts
    added 2 main scripts, a player input script and an entity controller script which gets variables passed into it from the player input script. also modified the myles test scene with a primitive player and the previously mentioned scripts

---
+++ Assets/Scripts/MOMovementController.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MOMovementController : MonoBehaviour
+{
+    private GameObject scriptEntity;
+    public int entityID = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        //check what entity the script is attached to used for debugging and testing reasons
+        if (entityID == 1)
+        {
+            Debug.Log("Contoller working for player with name " + gameObject.name);
+            scriptEntity = GameObject.FindGameObjectWithTag("Player");
+        }
+        else if (entityID == 2)
+        {
+            Debug.Log("Controller working for enemy with name " + gameObject.name);
+        }
+        else
+        {
+            Debug.Log("Controller not working for " + gameObject.name);
+        }
+    }
+
+    // method is called when needed from an input script
+    public void Move(Vector3 mov, float speed)
+    {
+        //move the gameobject based on the vars from the input script
+        scriptEntity.transform.parent.Translate(mov * speed * Time.deltaTime);
+        if (mov.x<0 & mov.z ==0) //left
+        {
+            scriptEntity.transform.rotation = Quaternion.Euler(gameObject.transform.rotation.x,180*Time.deltaTime,gameObject.transform.rotation.z);
+        }
+        else if (mov.x> 0 & mov.z == 0)//right
+        {
+            scriptEntity.transform.rotation = Quaternion.Euler(gameObject.transform.rotation.x, 0 * Time.deltaTime, gameObject.transform.rotation.z);
+        }
+        else if (mov.z > 0 & mov.x == 0)//up
+        {
+            scriptEntity.transform.rotation = Quaternion.Euler(gameObject.transform.rotation.x, 270 * Time.deltaTime, gameObject.transform.rotation.z);
+        }
+        else if (mov.z< 0 & mov.x == 0)//down
+        {
+            scriptEntity.transform.rotation = Quaternion.Euler(gameObject.transform.rotation.x, 90 * Time.deltaTime, gameObject.transform.rotation.z);
+        }
+        else if (mov.x <0 & mov.z>0)//up and left
+        {
+            scriptEntity.transform.rotation = Quaternion.Euler(gameObject.transform.rotation.x, 225 * Time.deltaTime, gameObject.transform.rotation.z);
+        }
+        else if (mov.x > 0 & mov.z > 0)//up and right
+        {
+            scriptEntity.transform.rotation = Quaternion.Euler(gameObject.transform.rotation.x, 315 * Time.deltaTime, gameObject.transform.rotation.z);
+        }
+        else if (mov.x < 0 & mov.z < 0)//down and left
+        {
+            scriptEntity.transform.rotation = Quaternion.Euler(gameObject.transform.rotation.x, 135 * Time.deltaTime, gameObject.transform.rotation.z);
+        }
+        else if (mov.x > 0 & mov.z < 0)//down and right
+        {
+            scriptEntity.transform.rotation = Quaternion.Euler(gameObject.transform.rotation.x, 45 * Time.deltaTime, gameObject.transform.rotation.z);
+        }
+    }
+
+    //called from input controller 
+    public void Jump(float height)
+    {
+        Debug.Log("game object jumping");
+        scriptEntity.GetComponent<Rigidbody>().AddForce(0, height, 0);
+        //jump animation and stuff here?
+    }
+
+    //called from input controller
+    public void Attack ()
+    {
+        Debug.Log("game object attacking");
+        //attack animation and stuff here?
+    }
+
+    //called from player's input controller only
+    public void Magic()
+    {
+        Debug.Log("game object using magic");
+        //check for different players 
+        /*
+        if (gameObject.name == "")
+        {
+        //use the magic for player 1
+        }
+        else if (gameObject.name == "")
+        {
+        //use the magic for player 2
+        }
+        else if (gameObject.name == "")
+        {
+        use the magic for player 3
+        }
+        */
+    }
+}
---
+++ Assets/Scripts/MOPlayerInputController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class MOPlayerInputController : MonoBehaviour
+{
+
+    public GameObject Player; // the player to be controlled
+    public Camera gameCamera; // the games main camera
+
+    private Vector3 gameCameraForward; // the forward direction of the game camera
+    private Vector3 moveDirection; // the direction the player will be moved in
+    private float moveSpeed; //the speed the player will move
+    private float jumpHeight; //the height of the players jump
+
+    // Use this for initialization
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player"); // set the player var to be the player
+        gameObject.GetComponent<MOMovementController>().entityID = 1;
+        moveSpeed = 5.0f;
+        jumpHeight = 500.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //check for the jump button
+        if (Input.GetButtonDown("Jump Button"))
+        {
+            // make player jump
+            Debug.Log("jump key pressed");
+            Player.GetComponent<MOMovementController>().Jump(jumpHeight);
+        }
+        //check for attack button
+        if (Input.GetButtonDown("Attack Button"))
+        {
+            //make player attack
+            Debug.Log("Attack key pressed");
+            Player.GetComponent<MOMovementController>().Attack();
+        }
+
+        //check for magic button
+        if (Input.GetButtonDown("Magic Button"))
+        {
+            Debug.Log("magic key pressed");
+            Player.GetComponent<MOMovementController>().Magic();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        //get the input for horizontal and vertical axis through unity controls
+        float hMov = CrossPlatformInputManager.GetAxis("Horizontal");
+        float vMov = CrossPlatformInputManager.GetAxis("Vertical");
+
+        //calculate movement relative to the player
+        moveDirection = vMov * Vector3.forward + hMov * Vector3.right;
+
+        //call the method on the controller script sending the required vars
+        Player.GetComponent<MOMovementController>().Move(moveDirection, moveSpeed);
+    }
+}
