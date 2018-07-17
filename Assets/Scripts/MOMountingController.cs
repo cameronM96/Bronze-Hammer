c7df41bf3b228f980ce370f4c76aff48a6102e07
Myles Okorn <1009099@student.sae.edu.au>
2018-07-17 11:52:10 +1000

    rewrite of mount script
    added a primitive mounting script, fixed bugs in mounting script, updated myles test scene

--- Assets/Scripts/MOMountingController.cs
+++ Assets/Scripts/MOMountingController.cs
@@ -2,28 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MOMountingController : MonoBehaviour {
-    private GameObject mount;
+public class MOMountingController : MonoBehaviour
+{
     private GameObject mountedCharacter;
-    private int mountedEntityID = 3;
 
-	// Use this for initialization
-	void Start ()
+    public bool isCurrentlyMounted;
+
+    // Use this for initialization
+    void Start()
     {
-        mount = gameObject;
-	}
-	
-	// Update is called once per frame
-	void Update ()
+        isCurrentlyMounted = false;
+        //gameObject.transform.parent = null;
+    }
+
+    private void Update()
     {
-		
-	}
+
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("Being mounted by " + collision.collider);
-        mountedCharacter = collision.collider.gameObject;
-        mountedCharacter.GetComponent<MOMovementController>().enabled = false;
-        mount.GetComponent<MOMovementController>().enabled = true;
+        //check so that you can have 2 mounts ride each other. 
+        if (collision.gameObject.tag != "Mount")
+        {
+            //check not already mounted by something
+            if (!isCurrentlyMounted)
+            {
+                Debug.Log(gameObject.name+" Being mounted by " + collision.collider);
+                //set mounted character to the object collided with
+                mountedCharacter = collision.collider.gameObject;
+
+                //set collided with game object as the mounts parent
+                gameObject.transform.parent = mountedCharacter.transform;
+
+                //adjust the bool
+                isCurrentlyMounted = true;
+
+                //adjust mounts position to appear under character as if riding
+                gameObject.transform.position = new Vector3(mountedCharacter.transform.position.x, 0.5f, mountedCharacter.transform.position.z);
+                gameObject.transform.Rotate(0, mountedCharacter.transform.rotation.w, 0);
+            }
+        }
     }
 }
