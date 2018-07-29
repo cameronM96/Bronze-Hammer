f2fee75c200d07c8440265b69388b57a39f00ee3
1009721 <39684519+1009721@users.noreply.github.com>
2018-07-30 00:04:29 +1000

    Pause Menu and Coontroller Prefabs

---
+++ Assets/Scripts/Menus/PauseMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+
+    public Transform PauseBackground;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (PauseBackground.gameObject.activeInHierarchy == false)
+                {
+                PauseBackground.gameObject.SetActive(true);
+                }
+            else
+            {
+                PauseBackground.gameObject.SetActive(false);
+            }
+        }
+    }
+
+	public void Quit ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 0);
+    }
+}
