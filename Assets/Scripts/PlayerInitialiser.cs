5e105621063e8d4f3f7685cbf726f29249d66c91
cameronM96 <37386594+cameronM96@users.noreply.github.com>
2018-07-31 09:33:34 +1000

    Character Initialiser

---
+++ Assets/Scripts/PlayerInitialiser.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInitialiser : MonoBehaviour {
+
+    private SelectedPlayer selectedPlayer;
+    [SerializeField] private GameObject estoc;
+    [SerializeField] private GameObject lilith;
+    [SerializeField] private GameObject crag;
+
+	// Use this for initialization
+	void Start ()
+    {
+        selectedPlayer = GameObject.FindGameObjectWithTag("Character Selector").GetComponent<SelectedPlayer>();
+
+        if (selectedPlayer.estoc)
+            SpawnEstoc();
+
+        if (selectedPlayer.lilith)
+            SpawnLilith();
+
+        if (selectedPlayer.crag)
+            SpawnCrag();
+	}
+
+    void SpawnEstoc ()
+    {
+        estoc.SetActive(true);
+        lilith.SetActive(false);
+        crag.SetActive(false);
+        Camera.main.GetComponent<Camera_Follow>().target = estoc.transform;
+    }
+
+    void SpawnLilith ()
+    {
+        estoc.SetActive(false);
+        lilith.SetActive(true);
+        crag.SetActive(false);
+        Camera.main.GetComponent<Camera_Follow>().target = lilith.transform;
+    }
+
+    void SpawnCrag()
+    {
+        estoc.SetActive(false);
+        lilith.SetActive(false);
+        crag.SetActive(true);
+        Camera.main.GetComponent<Camera_Follow>().target = crag.transform;
+    }
+}
---
+++ Assets/Scripts/SelectedPlayer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectedPlayer : MonoBehaviour {
+
+    public bool estoc = true;
+    public bool lilith;
+    public bool crag;
+
+    private static bool created = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+        if (!created)
+        {
+            DontDestroyOnLoad(this.gameObject);
+            created = true;
+        }
+	}
+
+    public void EstocSelected ()
+    {
+        estoc = true;
+        lilith = false;
+        crag = false;
+    }
+
+    public void LilithSelected()
+    {
+        estoc = false;
+        lilith = true;
+        crag = false;
+    }
+
+    public void CragSelected()
+    {
+        estoc = false;
+        lilith = false;
+        crag = true;
+    }
+}
